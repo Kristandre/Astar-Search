3867a8489b3f7c84205429c218a3f780c41548f5
Kristoffer <krisrand@stud.ntnu.no>
2015-10-06 17:24:36 +0200

    Made MapRenderer more readable

--- src/mapRenderer.java
+++ src/mapRenderer.java
@@ -1,27 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.List;
-import java.util.*;
 
 /**
  * Created by Kristoffer on 01/10/2015.
  */
-public class MapRenderer extends JPanel {
+class MapRenderer extends JPanel {
     java.util.List<String> map;
     java.util.List<Node> path, closed, open;
-    int height, width;
+    int height, width, squareSize, pathSize;
     boolean renderLists;
     Astar a;
 
     public MapRenderer(java.util.List<String> map, java.util.List<java.util.List<Node>> result, boolean renderLists){
         this.renderLists = renderLists;
         this.map = map;
+        this.squareSize = 30;
+        this.pathSize = 5;
         this.path = result.get(0);
         this.open = result.get(1);
         this.closed = result.get(2);
         a = new Astar();
-        this.height = map.size()*30+40;
-        this.width = map.get(0).length()*30;
+        this.height = map.size()*squareSize+40;
+        this.width = map.get(0).length()*squareSize+12;
     }
     public void paintComponent (Graphics g){
         super.paintComponent(g);
@@ -34,133 +34,133 @@ public class MapRenderer extends JPanel {
                 char c = line.charAt(j);
                 if(c == '.'){
                     g.setColor(Color.WHITE);
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == '#'){
                     g.setColor(Color.BLACK);
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == 'A'){
                     g.setColor(Color.BLUE);
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == 'B'){
                     g.setColor(Color.RED);
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == '+') {
                     g.setColor(Color.YELLOW);
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == 'w'){
                     g.setColor(new Color(83,118,176));
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == 'm'){
                     g.setColor(Color.GRAY);
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == 'f'){
                     g.setColor(new Color(15,94,11));
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == 'g'){
                     g.setColor(new Color(77,232,152));
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }else if(c == 'r'){
                     g.setColor(new Color(102,88,51));
-                    g.fillRect(j * 30, i * 30, 30, 30);
+                    g.fillRect(j * squareSize, i * squareSize, squareSize, squareSize);
                     if(a.coordInList(path,j,i)){
                         g.setColor(Color.BLACK);
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(open,j,i)){
                         g.setColor((Color.YELLOW));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }else if(renderLists && a.coordInList(closed,j,i)){
                         g.setColor((new Color(175,14,175)));
-                        g.fillRect(j * 30+12, i * 30+12, 5, 5);
+                        g.fillRect(j * squareSize+12, i * squareSize+12, pathSize, pathSize);
                     }
                 }
             }
@@ -176,3 +176,4 @@ public class MapRenderer extends JPanel {
         f.setVisible(true);
     }
 }
+
